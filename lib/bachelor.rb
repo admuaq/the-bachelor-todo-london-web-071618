034aff18d13067dd62d18364673373dd7f781320
Adham Muhammad <admuaq@gmail.com>
2018-07-18 00:05:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,22 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  
+    winner = data[season].detect {|person| person['status'] == 'Winner'}
+    winner['name'].split(" ")[0]
+    
 end
+
 
 def get_contestant_name(data, occupation)
   # code here
+    data.collect do |x|
+    person = x[1].detect {|y| y['occupation'] == occupation}
+      x[1][1]['name']
+      #binding.pry
+    end 
+  
 end
 
 def count_contestants_by_hometown(data, hometown)

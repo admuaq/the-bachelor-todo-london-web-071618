5300e4d16e571ced43f5edd7e8993d4203debf29
Adham Muhammad <admuaq@gmail.com>
2018-07-18 00:18:29 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,6 +14,7 @@ def get_contestant_name(data, occupation)
     contestants_name = ""
     data.collect do |x|
       bypass_season_data = x[1]
+      
       binding.pry
     end 
   contestants_name
